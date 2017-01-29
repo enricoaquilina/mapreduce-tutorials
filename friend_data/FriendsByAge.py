c6df3fccdeca879b0e66047f2dbe2d8eabf1640b
enricoaquilina <tappiera00@gmail.com>
2017-01-29 18:07:23 +0100

    first commit

---
+++ friend_data/FriendsByAge.py
@@ -0,0 +1,20 @@
+from mrjob.job import MRJob
+
+class MRRatingCounter(MRJob):
+    def mapper(self, key, line):
+        (userID, userName, age, noOfFriends) = line.split(',')
+        yield int(age), int(noOfFriends)
+
+    def reducer(self, age, noOfFriends):
+        # the looping across the various ages is implicit..we define what we do with each age
+        # age#1: 45, 23, age#2: 90, 20, age#3: 100, 2
+        total = 0
+        numElements = 0
+        for x in noOfFriends:
+            total += x
+            numElements += 1
+
+        yield age, total / numElements
+
+if __name__ == '__main__':
+    MRRatingCounter.run()
